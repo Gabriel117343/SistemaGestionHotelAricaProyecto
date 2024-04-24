d93c3fa5dbeacfe650e0622c96c940e6da8e8fad
Gabriel S <144576892+Gabriel117343@users.noreply.github.com>
2024-04-24 13:13:56 -0400

    Informaciones adicionales

--- HotelAricaProyecto/formulario/settings.py
+++ HotelAricaProyecto/formulario/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+# Repositorio del proyecto @Gabriel17343
 """
 
 from pathlib import Path
