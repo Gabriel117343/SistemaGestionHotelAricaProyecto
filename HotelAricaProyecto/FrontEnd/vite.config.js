1146ebfe3ae8b02f61585c801ebefa42963dde2c
Gabriel S <144576892+Gabriel117343@users.noreply.github.com>
2024-04-24 13:15:02 -0400

    Informaciones adicionales

--- HotelAricaProyecto/FrontEnd/vite.config.js
+++ HotelAricaProyecto/FrontEnd/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+//# Repositorio del proyecto @Gabriel17343
 export default defineConfig({
   plugins: [react()],
 })
